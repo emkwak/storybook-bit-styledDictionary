3253d2e9c7b220fa8f2b5808e19629c25c585e2e
Emily <emilyk@nutranext.net>
2020-06-05 12:28:10 -0400

    create app story and add buttonc component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
 import React from "react";
+import { Button } from "./components/button/button";
 
 function App() {
   return (
     <div className="App">
-      <h1>hello</h1>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "space-evenly",
+          flexWrap: "wrap",
+        }}
+      >
+        <Button variant="contained">Default</Button>
+        <Button variant="contained" color="primary">
+          Primary
+        </Button>
+        <Button variant="contained" color="secondary">
+          Secondary
+        </Button>
+        <Button variant="text">Default</Button>
+        <Button variant="text" color="primary">
+          Primary
+        </Button>
+        <Button variant="outlined">Default</Button>
+        <Button variant="outlined" color="tertiary">
+          Hello
+        </Button>
+      </div>
     </div>
   );
 }
