7567e8e8c05f4b961192fd0e29e0e27784c91834
Emily <emilyk@nutranext.net>
2020-06-08 18:44:10 -0400

    update button import

--- src/components/button/button.stories.js
+++ src/components/button/button.stories.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { action } from "@storybook/addon-actions";
-import { Button } from "./button";
+import Button from "./button";
 
 export default {
   title: "Button",
