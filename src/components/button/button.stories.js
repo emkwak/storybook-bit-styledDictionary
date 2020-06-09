542855fe5e700ce4752b0cae2e167ff34c8807f1
Emily <emilyk@nutranext.net>
2020-06-09 09:48:00 -0400

    remove unused stories and update button story

--- src/components/button/button.stories.js
+++ src/components/button/button.stories.js
@@ -7,7 +7,7 @@ export default {
   component: Button,
 };
 
-export const Basic = () => (
+export const Contained = () => (
   <div
     style={{
       display: "flex",
@@ -18,11 +18,54 @@ export const Basic = () => (
     <Button onClick={action("button-clicked")} variant="contained">
       Contained
     </Button>
+    <Button variant="contained" color="primary">
+      Primary
+    </Button>
+    <Button variant="contained" color="secondary">
+      Secondary
+    </Button>
+  </div>
+);
+
+export const Text = () => (
+  <div
+    style={{
+      display: "flex",
+      flexDirection: "row",
+      justifyContent: "space-evenly",
+    }}
+  >
     <Button onClick={action("button-clicked")} variant="text">
       Text
     </Button>
+    <Button variant="text" color="primary">
+      Primary
+    </Button>
+    <Button variant="text" color="secondary">
+      Secondary
+    </Button>
+  </div>
+);
+
+export const Outlined = () => (
+  <div
+    style={{
+      display: "flex",
+      flexDirection: "row",
+      justifyContent: "space-evenly",
+    }}
+  >
     <Button onClick={action("button-clicked")} variant="outlined">
       Outlined
+    </Button>
+    <Button variant="outlined" color="primary">
+      Primary
+    </Button>
+    <Button variant="outlined" color="secondary">
+      Secondary
+    </Button>
+    <Button variant="outlined" color="tertiary">
+      Hello
     </Button>
   </div>
 );
